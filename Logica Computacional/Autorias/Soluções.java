37c204c2d3b65a1f3d88d4ff299a9cafd98133cc
Pedro Nieto <pedronieto.2005@gmail.com>
2022-04-27 12:23:12 -0300

    Update Soluções.java

--- Logica Computacional/Autorias/Soluções.java
+++ Logica Computacional/Autorias/Soluções.java
@@ -1,3 +1,5 @@
+//Feito no Visual Studio Code (VS Code)
+
 import javax.swing.JOptionPane;
 import java.text.DecimalFormat;
 
@@ -17,8 +19,8 @@ public class Soluções{
 
         //Concentração Comum (C) ///////////////////////////////////////////////////////////////////////////////
         if(escolha.equals("Concentração Comum") || escolha.equals("C")){
-            double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-            double V = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'V' em litros", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+            double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+            double V = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'V' em litros", "Inserção", JOptionPane.PLAIN_MESSAGE));
             double C = m1/V;
             JOptionPane.showMessageDialog(null, "O valor da concentração comum é: "+C+" g/L", "Resultado", JOptionPane.INFORMATION_MESSAGE);}
         ///////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -27,8 +29,8 @@ public class Soluções{
 
         //Título (T e T%) ///////////////////////////////////////////////////////////////////////////////
         if(escolha.equals("Título") || escolha.equals("T")){
-            double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-            double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+            double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+            double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.PLAIN_MESSAGE));
             double T = m1/(m1+m2);
             double Tp = T*100;
             JOptionPane.showMessageDialog(null, "O valor do título é: "+fT.format(T)+" \nO valor do título em porcentagem é: "+fTp.format(Tp)+"%", "Resultado", JOptionPane.INFORMATION_MESSAGE);}
@@ -41,15 +43,15 @@ public class Soluções{
             String escolhaMr = JOptionPane.showInputDialog(null, "Selecione a fórmula que deseja utilizar: \nFórmula 1: n1/V \nFórmula 2: m1/M1*V", "Seleção", JOptionPane.INFORMATION_MESSAGE);
 
             if(escolhaMr.equals("Fórmula 1") || escolhaMr.equals("1") || escolhaMr.equals("n1/V")){
-                double n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                double V = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'V' em litros", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+                double n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                double V = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'V' em litros", "Inserção", JOptionPane.PLAIN_MESSAGE));
                 double Mr1 = n1/V;
                 JOptionPane.showMessageDialog(null, "O valor da molaridade é: "+fT.format(Mr1)+" mol/L", "Resultado", JOptionPane.INFORMATION_MESSAGE);}
 
             else if(escolhaMr.equals("Fórmula 2") || escolhaMr.equals("2") || escolhaMr.equals("m1/M1*V")){
-                double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                double M1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                double V = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'V' em litros", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+                double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                double M1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                double V = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'V' em litros", "Inserção", JOptionPane.PLAIN_MESSAGE));
                 double Mr2 = m1/(M1*V);
                 JOptionPane.showMessageDialog(null, "O valor da molaridade é: "+fT.format(Mr2)+" mol/L", "Resultado", JOptionPane.INFORMATION_MESSAGE);}}
         ////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -64,16 +66,16 @@ public class Soluções{
                 String escolhaX1 = JOptionPane.showInputDialog(null, "Selecione a fórmula que deseja utilizar: \nFórmula 1: n1/n \nFórmula 2: m1/M1 / m1/M1 + m2/M2", "Seleção", JOptionPane.INFORMATION_MESSAGE);
 
                 if(escolhaX1.equals("Fórmula 1") || escolhaX1.equals("1") || escolhaX1.equals("n1/n")){
-                    double n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                    double n2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n2'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+                    double n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                    double n2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n2'", "Inserção", JOptionPane.PLAIN_MESSAGE));
                     double X1a = n1/(n1+n2);
                     JOptionPane.showMessageDialog(null, "O valor da fração molar do soluto é: "+X1a, "Resultado", JOptionPane.INFORMATION_MESSAGE);}
                 
                 else if(escolhaX1.equals("Fórmula 2") || escolhaX1.equals("2") || escolhaX1.equals("m1/M1 / m1/M1 + m2/M2")){
-                    double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                    double M1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                    double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                    double M2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M2'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+                    double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                    double M1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                    double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                    double M2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M2'", "Inserção", JOptionPane.PLAIN_MESSAGE));
                     double X1b = (m1/M1)/((m1/M1)+(m2/M2));
                     JOptionPane.showMessageDialog(null, "O valor da fração molar do soluto é: "+X1b, "Resultado", JOptionPane.INFORMATION_MESSAGE);}}
 
@@ -81,16 +83,16 @@ public class Soluções{
                 String escolhaX2 = JOptionPane.showInputDialog(null, "Selecione a fórmula que deseja utilizar: \nFórmula 1: n2/n \nFórmula 2: m2/M2 / m1/M1 + m2/M2", "Seleção", JOptionPane.INFORMATION_MESSAGE);
 
                 if(escolhaX2.equals("Fórmula 1") || escolhaX2.equals("1") || escolhaX2.equals("n2/n")){
-                    double n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                    double n2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n2'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+                    double n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                    double n2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n2'", "Inserção", JOptionPane.PLAIN_MESSAGE));
                     double X2a = n2/(n1+n2);
                     JOptionPane.showMessageDialog(null, "O valor da fração molar do soluto é: "+X2a, "Resultado", JOptionPane.INFORMATION_MESSAGE);}
 
                 else if(escolhaX2.equals("Fórmula 2") || escolhaX2.equals("2") || escolhaX2.equals("m2/M2 / m1/M1 + m2/M2")){
-                    double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                    double M1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                    double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                    double M2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M2'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+                    double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                    double M1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                    double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                    double M2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M2'", "Inserção", JOptionPane.PLAIN_MESSAGE));
                     double X2b = (m2/M2)/((m1/M1)+(m2/M2));
                     JOptionPane.showMessageDialog(null, "O valor da fração molar do soluto é: "+X2b, "Resultado", JOptionPane.INFORMATION_MESSAGE);}}}
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -103,15 +105,15 @@ public class Soluções{
             String escolhaW = JOptionPane.showInputDialog(null, "Selecione a fórmula que deseja utilizar: \nFórmula 1: n1/m2 \nFórmula 2: m1/M1*m2", "Seleção", JOptionPane.INFORMATION_MESSAGE);
 
             if(escolhaW.equals("Fórmula 1") || escolhaW.equals("1") || escolhaW.equals("n1/m2")){
-                double n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+                double n1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'n1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.PLAIN_MESSAGE));
                 double W_1 = n1/m2;
-                JOptionPane.showMessageDialog(null, "O valor da fração molar do soluto é: "+W_1+" mol/Kg", "Resultado", JOptionPane.INFORMATION_MESSAGE);}   
+                JOptionPane.showMessageDialog(null, "O valor da fração molar do soluto é: "+W_1+" mol/Kg", "Resultado", JOptionPane.PLAIN_MESSAGE);}   
     
             else if(escolhaW.equals("Fórmula 2") || escolhaW.equals("2") || escolhaW.equals("m1/M1*m2")){
-                double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                double M1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M1'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
-                double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.INFORMATION_MESSAGE));
+                double m1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                double M1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'M1'", "Inserção", JOptionPane.PLAIN_MESSAGE));
+                double m2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Insira o valor de 'm2'", "Inserção", JOptionPane.PLAIN_MESSAGE));
                 double W_2 = m1/(M1*m2);
                 JOptionPane.showMessageDialog(null, "O valor da fração molar do soluto é: "+W_2+" mol/Kg", "Resultado", JOptionPane.INFORMATION_MESSAGE);}}
         ///////////////////////////////////////////////////////////////////////////////////////////////////
